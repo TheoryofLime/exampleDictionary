53a6078ab6f96ce20c089395390531f76671af92
user <user@PC>
2023-03-12 19:42:41 -0400

    Add project files.

---
+++ exampleDictionary/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace exampleDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> newDictionary = new Dictionary<string, int>();
+            // add stuff
+            newDictionary.Add("A", 1);
+            newDictionary.Add("B", 2);
+            newDictionary.Add("C", 3);
+            newDictionary.Add("D", 4);
+            newDictionary.Add("E", 5);
+            // display all the keys
+            foreach (KeyValuePair<string, int> input in newDictionary) Console.WriteLine(input.Key);
+            // display all the values
+            foreach (KeyValuePair<string, int> input in newDictionary) Console.WriteLine(input.Value);
+            // both i guess
+            foreach (KeyValuePair<string, int> input in newDictionary) Console.WriteLine(input.Key, input.Value);
+            // remove
+            newDictionary.Remove("A");
+            // display count
+            Console.WriteLine(newDictionary.Count);
+        }
+    }
+}
